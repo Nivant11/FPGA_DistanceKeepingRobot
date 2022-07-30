398dd997c9e293917477a46ea00625a29937b0f2
nivant desai <nnd25@cornell.edu>
2022-07-30 14:18:12 -0400

    PWM test works for low duty cycles

--- DistanceKeepingRobot.srcs/sources_1/new/PWMController.v
+++ DistanceKeepingRobot.srcs/sources_1/new/PWMController.v
@@ -5,13 +5,14 @@ module PWMController(
     input [31:0] DIST,
     input CLK,
     input RESET,
-    output PWM
+    output PWM,
+    output wire integer DutyCycle
     );
     
     wire integer THRESHOLD = 38; //The threshold distance. Any distance below this defines a "trip" of the sensor
     wire integer error;
     wire integer Kp = 4; //Proportional constant
-    wire integer DutyCycle;
+    wire integer i_DutyCycle;
     wire integer pTerm;
     reg [16:0] counter = 0; //Used to set the PWM frequency
     
@@ -19,7 +20,8 @@ module PWMController(
     //PID Control. If the Distance is greater than the threshold (i.e. the sensor has not been "tripped", the DutyCycle is zero
     assign error = (DIST > 38) ? 0 : THRESHOLD - DIST;
     assign pTerm = Kp * error;
-    assign DutyCycle = (error == 0) ? 0 : (pTerm > 100) ? 100 : pTerm; //Set the duty cycle and clamp it to a range of 0 to 100
+    assign i_DutyCycle = (error == 0) ? 0 : (pTerm > 100) ? 100 : pTerm; //Set the duty cycle and clamp it to a range of 0 to 100
+    assign DutyCycle = i_DutyCycle;
     
     always @(posedge CLK) begin
         if(counter == 100) counter = 0; 
--- DistanceKeepingRobot.srcs/sources_1/new/PWMController_tb.v
+++ DistanceKeepingRobot.srcs/sources_1/new/PWMController_tb.v
@@ -6,14 +6,54 @@ module PWMController_tb();
 reg CLK = 1'b0;
 always #5 CLK = ~CLK;
 wire RESET = 1'b0; //Not used
-wire integer DIST = 30;
+wire integer DIST;
 wire PWM;
+wire integer DutyCycle;
+integer PWM_count = 0;
+integer test_number = 1;
+
+localparam CHANGING_DISTANCE = 1'b0;
+localparam MEASURING_PWM = 1'b1;
+reg test_State = CHANGING_DISTANCE;
+reg temp = 1'b0;
+
+//Check the pulse width of the PWM signal, should correspond to the Duty Cycle
+always @(posedge CLK) begin
+    if(~PWM) begin
+        PWM_count = 0;
+    end
+    if(PWM) begin
+        PWM_count = PWM_count+1;
+    end
+   
+    //Self-checking test FSM
+    if(test_State == CHANGING_DISTANCE) begin
+        temp = 1'b0;
+        if(PWM) begin
+            test_State = MEASURING_PWM;
+        end
+    end
+    else if(test_State == MEASURING_PWM) begin
+        if(~PWM) begin
+            temp = 1'b1;
+            $display("PWM_count: %d", PWM_count);
+            if(test_number < 5) begin
+                test_number = test_number +1;
+            end
+            test_State = CHANGING_DISTANCE;
+        end
+    end
+end
+
+assign DIST = (test_number == 1) ? 29 : (test_number == 2) ? 30 : (test_number == 3) ? 21 :
+            (test_number == 4) ? 40 : (test_number == 5) ? 37 : 31;
 
 PWMController uut(
 .CLK(CLK),
 .RESET(RESET),
 .DIST(DIST),
-.PWM(PWM)
+.PWM(PWM),
+.DutyCycle(DutyCycle)
 );
 
 endmodule
